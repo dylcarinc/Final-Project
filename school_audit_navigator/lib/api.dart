2a6a421543946c09bb90c3f2692ccfffd0866163
Dylan Carter <carterdw@hendrix.edu>
2024-11-06 10:22:54 -0600

    added more to API class

--- school_audit_navigator/lib/api.dart
+++ school_audit_navigator/lib/api.dart
@@ -1,10 +1,29 @@
 
 
 import 'package:http/http.dart' as http;
-void showData() async {
-var url = Uri.https('api-staging.fac.gov', '/general',{'auditee_zip':'eq.72032'});
+void searchColleges(bool isHigerED, String state) async {
+  var url = Uri.https('google.com');
+if (isHigerED){
+  url = Uri.https('api-staging.fac.gov', '/general',{'select': 'auditee_name','auditee_state':'eq.$state', 'auditee_name': 'fts.university', 'auditee_name': 'fts.college'},);
+}
+else{
+ url = Uri.https('api-staging.fac.gov', '/general',{'select': 'auditee_name', 'auditee_state':'eq.$state', 'auditee_name': 'fts.school'});
+}
 print(url);
 var response = await http.get(url, headers: {'X-Api-Key': 'OTOlQu3kFOeDM2LwYz7S0ofa3m45FJQOhfB40VEz'});
 print('Response status: ${response.statusCode}');
 print('Response body: ${response.body}');
+
+}
+void getCollegeYear(String name, int year) async{
+  var url = Uri.https('api-staging.fac.gov', '/general',{'select': 'auditee_name','auditee_name':'eq.$name', 'audit_year':'$year'});
+  var response = await http.get(url, headers: {'X-Api-Key': 'OTOlQu3kFOeDM2LwYz7S0ofa3m45FJQOhfB40VEz'});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
+}
+void getCollegeInfo(String id) async{
+  var url = Uri.https('api-staging.fac.gov', '/federal_awards',{'General.report_id':'$id'});
+  var response = await http.get(url, headers: {'X-Api-Key': 'OTOlQu3kFOeDM2LwYz7S0ofa3m45FJQOhfB40VEz'});
+  print('Response status: ${response.statusCode}');
+  print('Response body: ${response.body}');
 }
