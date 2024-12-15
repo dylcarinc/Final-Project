f1b91df0d993f14c9db6d1fae600690111885559
Jayden Grisby <Jayden.Grisby@gmail.com>
2024-12-14 19:34:17 -0600

    Fixed app crashing if audit from 2021 was selected

--- school_audit_navigator/lib/audit_page.dart
+++ school_audit_navigator/lib/audit_page.dart
@@ -39,7 +39,7 @@ class _AuditPageState extends State<AuditPage> {
       children: [
         DropdownButton(
                   value: dropdownValue,
-                  items: <String>['2016','2017','2018', '2019', '2020', ' 2021', '2022', '2023','2024']
+                  items: <String>['2016','2017','2018', '2019', '2020', '2021', '2022', '2023','2024']
                       .map((String value) {
                     return DropdownMenuItem<String>(
                       value: value,
